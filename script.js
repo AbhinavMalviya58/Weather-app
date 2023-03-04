5d51a3481bd589121b9437688e389d4223e300a7
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2023-03-05 00:22:12 +0530

    update 05032023

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const options = {
 	}
 };
 const getWeather = (city) => {
-	
-	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Gorakhpur', options)
+	cityName.innerHTML = city
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
 	.then(response => response.json())
 	.then((response) => {
 		
@@ -16,11 +16,14 @@ const getWeather = (city) => {
 		
 		cloud_pct.innerHTML = response.cloud_pct
 		temp.innerHTML = response.temp
+		temp2.innerHTML = response.temp
 		feels_like.innerHTML = response.feels_like
 		humidity.innerHTML = response.humidity
+		humidity2.innerHTML = response.humidity
 		min_temp.innerHTML = response.min_temp
 		max_temp.innerHTML = response.max_temp
 		wind_speed.innerHTML = response.wind_speed
+		wind_speed2.innerHTML = response.wind_speed
 		wind_degrees.innerHTML = response.wind_degrees
 		sunrise.innerHTML = response.sunrise
 		sunset.innerHTML = response.sunset
@@ -29,5 +32,8 @@ const getWeather = (city) => {
 }
 
 submit.addEventListener("click", (e)=>{
+	e.preventDefault()
 	getWeather(city.value)
 })
+
+getWeather("Delhi")
